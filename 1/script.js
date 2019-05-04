46c1d8b7ebc48a24264a95e7b97fd5dc7c4d1733
Your Name <you@example.com>
2019-05-04 14:22:12 +0300

    добавил еще один вопрос

--- 1/script.js
+++ 1/script.js
@@ -1,18 +1,20 @@
-
-let money=+prompt('Ваш бюджет на месяц?','0');
-let time=prompt('Введите дату в формате YYYY-MM-DD','');
-let appData={
-    budget:money,
-    timeData:time,
-    expenses:{},
+let money = +prompt('Ваш бюджет на месяц?', '0');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
     optoinalExpenses: {},
     income: [],
     savings: false
 };
-let dopRashod=+prompt('Введите обязательную строку расходов в этом месяце');
-let moneyRashod=+prompt('Во сколько это обойдется?');
+let dopRashod1 = +prompt('Введите обязательную строку расходов в этом месяце');
+let moneyRashod1 = +prompt('Во сколько это обойдется?');
+let dopRashod2 = +prompt('Введите обязательную строку расходов в этом месяце');
+let moneyRashod2 = +prompt('Во сколько это обойдется?');
 
-appData.expenses.dopRashod=moneyRashod;
-    
 
-    alert("Бюджет на месяц - "+(appData.budget-moneyRashod)/30);
+appData.expenses.dopRashod1 = moneyRashod1;
+appData.expenses.dopRashod2 = moneyRashod2;
+
+alert("Бюджет на месяц - " + (appData.budget) / 30);
